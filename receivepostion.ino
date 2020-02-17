41b5c86540cb18ea11b8e0fec63d14dcc23560ab
ColeMcKechney <61060833+ColeMcKechney@users.noreply.github.com>
2020-02-17 11:18:30 -0600

    Create receivepostion.ino

---
+++ receivepostion.ino
@@ -0,0 +1,24 @@
+uint8_t buf[3];
+
+uint8_t d1; 
+uint8_t d2; 
+uint16_t wd;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available())
+{
+  Serial.readBytes(buf,2);
+ // Serial.println(var3);
+// Serial.print(buf[0]);
+   // Serial.print(buf[1]);
+ d1=buf[5]; 
+ d2=buf[6]; 
+ wd = (d2 << 8) | d1;
+ Serial.print(wd);
+}
